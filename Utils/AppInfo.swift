92094765b5c35bbfd3eb50e9123ec81030efb0c8
Mr B <houseofbartzzz@gmail.com>
2026-02-07 22:06:06 +0100

    Point app support links to new GitHub Pages

--- Utils/AppInfo.swift
+++ Utils/AppInfo.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 enum AppInfo {
-    static let privacyURL = URL(string: "https://mrbartizta.github.io/MyAppSupportIOS/privacy.html")!
-    static let supportURL = URL(string: "https://mrbartizta.github.io/MyAppSupportIOS/support.html")!
+    static let privacyURL = URL(string: "https://mrbartizta.github.io/git-github.com-you-ChronoFlowFocusTimer/privacy.html")!
+    static let supportURL = URL(string: "https://mrbartizta.github.io/git-github.com-you-ChronoFlowFocusTimer/support.html")!
 }
